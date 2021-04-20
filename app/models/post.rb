4d9f5b7ab2d1751014e42fd9d6d9a6b04e56c3a1
julianxlozano <julianxlozano@gmail.com>
2021-04-20 15:33:40 -0500

    commit tree d759a8776956e3a2ae50d76cc897b5b50aed0669

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ActiveRecord::Base
+   
+    validates :title, presence: true
+    validates :content, length: {minimum: 10}
+    validates :summary, length: {maximum: 10}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :title_clickbait_checker
+
+    CLICKBAIT = [
+        "Won't Believe",
+        "Secret",
+        "Top[number]",
+        "Guess"
+    ]
+
+    def title_clickbait_checker
+        if title.present? && CLICKBAIT.none? {|p| title.match(p)}
+            errors.add(:title, "title not clickbait-y enough")
+        end
+    end
+
 end
